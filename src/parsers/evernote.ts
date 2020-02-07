f6972169eb85dac1e9761d274538322ee14228c2
Dexter St Pierre <dexter.stpierre.dev@gmail.com>
2020-02-06 22:05:47 -0600

    Evernote parser working

--- src/parsers/evernote.ts
+++ src/parsers/evernote.ts
@@ -1,3 +1,15 @@
-export const evernoteParser = () => {
+const extractBody = (html: string): string => {
+  const [, withoutBeginning] = html.split('<div class="page-body">');
+  return withoutBeginning;
+}
+
+const convertHighlights = (html: string): string => {
+  const replacedHighlights = html.replace(/<span style="background-color: rgb\(\d*, \d*, \d*\);-evernote-highlight:true;">(.*?)<\/span>/g, '^^$1^^');
+  return replacedHighlights;
+}
+
+export const evernoteParser = (html: string) => {
   console.log('evernote parser')
+  const convertedHightlights = convertHighlights(html);
+  return convertedHightlights;
 }
